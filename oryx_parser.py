b67017bf0fd565031d086ef80a6721c94afb0f80
JasonWu00 <zehong.wu@macaulay.cuny.edu>
2023-09-13 16:18:17 -0400

    completed work up to 'parse data from proof links' part

--- oryx_parser.py
+++ oryx_parser.py
@@ -1,12 +1,87 @@
 """
-This file seeks to scrape data from the Oryx blog on RU and UA losses and store the scraped data in one or more CSV files.
+This file seeks to scrape data from the Oryx blog on RU and UA losses
+and store the scraped data in one or more CSV files.
 """
 
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 import re
 
+# A dictionary of keywords that appear in flag display links and their corresponding country abbr.
+manufacturer_dict = {
+    "Russia": "RU",
+    "Soviet_Union": "USSR",
+    "Italy": "ITA",
+    "France": "FR",
+    "Spain": "SP",
+    "United_States": "USA",
+    "Ukraine": "UA",
+    "Sweden": "SWE",
+    "Finland": "FIN",
+    "Poland": "PL",
+    "Israel": "ISR",
+    "Iran": "IR",
+    "Czech_Republic": "CZ",
+    "Germany": "GER",
+    "Belarus": "BEL",
+}
+
+vehicle_types = {"Unknown T-54/55": "Tanks",
+                 "BMPT Terminator": "Armored Fighting Vehicles",
+                 "BTR-60PB": "Armoured Personnel Carriers",
+                 "BMP-1KSh command and staff vehicle": "Command Posts and Communications Stations",
+                 "UR-67 mine clearing charge on BTR-D APC": "Engineering Vehicles and Equipment",
+                 "9P148 Konkurs": "Self-Propelled Anti-Tank Missile Systems",
+                 "1V110 BM-21 Grad battery command vehicle": "Artillery Support Vehicles and Equipment",
+                 "82mm 2B9 Vasilek automatic gun mortar": "Towed Artillery",
+                 "120mm 2S9 Nona": "Self Propelled Artillery",
+                 "122mm BM-21 Grad": "Multiple Rocket Launchers",
+                 "23mm ZU-23-2": "Anti-Aircraft Guns",
+                 "3 BTR-ZD Skrezhet": "Self-Propelled Anti-Aircraft Guns",
+                 "9K33 Osa": "Surface-To-Air Missile Systems",
+                 "9S36 (for Buk-M2)": "Radars",
+                 "R-325BMV jamming station": "Jammers and Deception Systems",
+                 "MiG-31BM fighter aircraft": "Aircraft",
+                 "Mi-8 transport helicopter": "Helicopters",
+                 "Orion": "Unmanned Combat Aerial Vehicles",
+                 "Forpost": "Reconnaissance Unmanned Aerial Vehicles",
+                 "Project 1164 Slava-class guided missile cruiser": "Naval Ships and Submarines",
+                 "GAZ-51": "Trucks, Vehicles, and Jeeps"}
+"""
+A dictionary of the first entries of vehicle types and their corresponding types.
+The Oryx blog has some extremely inconsistent tag, id, and class usage which
+makes code-based acquiring of this data too difficult.
+"""
+
+df = pd.DataFrame(columns=["class", "type", "status", 
+                           "year", "month", "day", 
+                           "manufacturer", "owner", "proof"])
+"""
+A DataFrame that will store all scraped vehicle loss data.
+
+Columns include:
+Class: vehicle designation (T-80BVM, BMP-1, Ka-52, Su-25, etc)
+Type: vehicle category (tank, helicopter, boat, etc)
+Status: type of loss (destroyed, abandoned, captured, etc)
+Year, Month, Day: date of vehicle loss
+Manufactuer: country that produced it (USSR, RU, etc)
+Owner: country that used it (UA or RU)
+Proof: postimg or twitter link that shows the loss.
+"""
+
+ru_losses = "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html"
+ua_losses = "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-ukrainian.html"
+
 def more_parsing(input_name: str) -> str:
+    """
+    This function takes in one input:
+    input_name: a partially parsed string containing the name of a type of vehicle.
+    They usually take the form of " [number] [name]".
+
+    This function removes leading spaces and number counts from the string
+    and returns the relevant portions "[name]".
+    """
     output = ""
     startReading = False
     for index in range(1,len(input_name)):
@@ -16,40 +91,53 @@ def more_parsing(input_name: str) -> str:
             output += input_name[index]
     return output[1:]
 
-ru_losses = "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html"
-ua_losses = "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-ukrainian.html"
-
-production_dict = {
-    "Russia": "Produced by RU",
-    "Soviet_Union": "Produced by USSR"
-}
-
 r = requests.get(ru_losses)
 soup = BeautifulSoup(r.content, 'html.parser')
+
+# The Oryx webpage has the main article contents stored under an <article> tag.
+# The <ul> <li> lists for each major vehicle type are not classed or id'd in any special way.
+# Thus it is necessary to do this inefficient setup.
 art = soup.find('article')
-s = art.find_all('ul')
-for type in s:
-    vehicles = type.find_all('li')
+lists = art.find_all('ul')
+vehicle_class = ""
+
+for vehicle_type in lists:
+    #sys.exit(0) # temporary exit code to not do extra work that I don't need to do yet
+    vehicles = vehicle_type.find_all('li')
+
     for vehicle in vehicles:
-        print(vehicle.text)
-        if "T-54/55" in vehicle.text: print("1 Unknown T-54/55")
-        # Source used to generate regex: https://regex-generator.olafneumann.org/
-        else:
-            parsed_names = re.search(r"\S[\w\s\(\)-]*", vehicle.text).group(0)
-            print(more_parsing(parsed_names))
+        #print(vehicle.text)
+        parsed_name = re.search(r"\S[\w\s\(\)\-\"\'\,]*", vehicle.text).group(0)
+        parsed_name = more_parsing(parsed_name)
+
+        if parsed_name in vehicle_types:
+            vehicle_class = vehicle_types[parsed_name]
+        
         flag = vehicle.find('img', class_='thumbborder')
+        flag_country = None
         if flag != None:
-            #print(flag)
-            print(flag.get('src'))
             flag_found = False
-            for target in production_dict:
+            for target in manufacturer_dict:
                 if target in flag.get('src'):
                     flag_found = True
-                    print(production_dict[target])
+                    flag_country = manufacturer_dict[target]
+                    print(flag_country)
                     break
             if not flag_found:
-                print("Flag not in production dict")
-            print()
-        #links = vehicle.find_all('a')
-        #for link in links:
-            #print(link.get('href'))
+                print("Flag not in manufacturer dict")
+        
+        proofs = vehicle.find_all('a')
+        for proof in proofs:
+            link = proof.get('src')
+            if "postimg" in link:
+                i = 0
+                # do postimg processing here
+            elif "twitter" in link:
+                i = 0
+                # do twitter processing here
+            else:
+                i = 0
+                # do third party link stuff here
+
+"""
+"""
